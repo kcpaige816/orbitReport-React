eb9e41abe7f4d23475ce0b2ec364f8dc3177eaa4
kcpage816 <kierstendotpaige@gmail.com>
2024-03-21 15:22:48 -0500

    finished tasks 4-6. project complete!

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ function App() {
   const [sat, setSat] = useState(satData);
   const displaySats = [...new Set(satData.map((data) => data.orbitType))];
   const filterByType = (currentType) => {
-    const displaySats = satData.filter((newSatDisplay) => {
+  const displaySats = satData.filter((newSatDisplay) => {
       return newSatDisplay.orbitType === currentType;
     });
     setSat(displaySats);
